c61cbe6fe10bb3ca4e94431e9ebe68356174c9bb
omarmolina-dev <omar03molina@gmail.com>
2026-03-27 15:18:16 -0700

    add user search with filter and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
 import { useState, useEffect } from "react";
 
 function App() {
-    const [rate, setRate] = useState(null);
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
-        fetch("https://api.frankfurter.app/latest?from=USD&to=MXN")
+        fetch("https://jsonplaceholder.typicode.com/users")
             .then(res => res.json())
-            .then(data => setRate(data.rates.MXN));
+            .then(data => {
+                setUsers(data);
+                setLoading(false);
+            });
     }, []);
+
+    <input value={search} onChange={(e) => setSearch(e.target.value)} />
+
+    const filteredUsers = users.filter(user => 
+        user.name.toLowerCase().includes(search.toLowerCase()));
+   
+    if (loading) return <p>Loading users...</p>;
 
     return (
         <div>
-            <h1>USD to MXN</h1>
-            {rate ? <p>1 USD = {rate} MXN</p> : <p>Loading...</p>}
+            <h1>Users</h1>
+            <input 
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search user..." />
+            {filteredUsers.map(user => (
+                <div key={user.id}>
+                    <h3>{user.name}</h3>
+                    <p>{user.email}</p>
+                </div>
+            ))}
         </div>
     );
+
 }
 
 export default App;
